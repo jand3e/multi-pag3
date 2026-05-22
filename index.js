b86496a6bd70af6f0f15b934a5f128f01e3381cd
Jande3e <johnderickgaviola@gmail.com>
2026-05-23 00:20:25 +0800

    donee

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// Use reportWebVitals so it's not an unused import. You can pass a logging function or leave blank.
+reportWebVitals();
